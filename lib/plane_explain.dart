f26f88ec5aeac07f14892105c3006a26f604a72c
keigo <b1951154@planet.kanazawa-it.ac.jp>
2021-09-17 21:02:33 +0900

    :sparkles:場所の説明文追加

--- lib/plane_explain.dart
+++ lib/plane_explain.dart
@@ -153,7 +153,7 @@ class _PicExplain extends State<PicExplain>{
 
                     child: Column(
                       children: <Widget>[
-                        Text("湯涌総湯",
+                        Text("氷室小屋",
                           style: TextStyle(
                             fontWeight: FontWeight.bold, fontSize: 36.0,),
                         ),
@@ -174,10 +174,49 @@ class _PicExplain extends State<PicExplain>{
                           ),
                           margin: EdgeInsets.only(top: 10),
 
-                          child: Text('養老二年（718年）近郷の農夫が泉に身を癒す白鷺をみてこの温泉を発見したと伝えられています。'
-                              '藩政時代は、加賀藩の歴代藩主を始め一族が常用し、その効能によって治癒本復することがしばしばあり、これを賞され湯宿の主人に名字帯刀が許されたと言われます。'
-                              '大正の初めドイツで開かれた万国鉱 泉博覧会に当時の内務省の推薦により日本の名泉として出展、泉質の良さが認められました。以来、文人墨客の来湯が繁くなり、特異な美人画で知られる大正の詩人、'
-                              '竹久夢二が愛する女性彦乃を至福の日々を過ごした「ロマンの湯」としても知られています。'
+                          child: Text('加賀藩の初期には白山山系の倉谷村の氷室の氷が使われていたが、五代藩主綱紀公の頃、金沢市近郊や市内に多くの氷室が設けられ、'
+                              '町民も氷を食する事が許された。しかし夏の氷は貴重品で、主に目上の人への贈答品などに使われ、庶民が食することは大変だったようであります'
+                          'それで氷の代わりに麦で作った「氷室饅頭」を食べて無病息災を願う習慣だけが現在でも残っております。'
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+
+                  /// 2つ目の場所
+
+                  Card(
+                    elevation: 30.0,
+                    color: Colors.white10,
+                    margin: EdgeInsets.all(10),
+
+                    child: Column(
+                      children: <Widget>[
+                        Text("みどりの里",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 36.0,),
+                        ),
+
+                        Container(
+                          width: deviceWidth*0.9,
+
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(50),
+                            child: Image.asset('assets/images/KeigoSirayu.png'),
+                          ),
+                        ),
+
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8.0),
+
+                          ),
+                          margin: EdgeInsets.only(top: 10),
+
+                          child: Text('緑豊かな自然環境の中で、野菜づくりや農林産物の加工その他の農林業に関する体験等を通して、自然に親しむことができます。'
+                              '農林業についての理解を深め、農林業の振興と周辺地域の活性化に資するための施設として、2002（平成14）年5月に開設されました。'
+                              'ここでは蕎麦打ち体験や味噌作り体験などの、普段ではなかなか目にすることができない事を体験できます'
                           ),
                         ),
                       ],
