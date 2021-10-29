8d12fc06506a5d9d37b0ca4e3cafa6a639330807
r0227n <b180318@planet.kanazawa-it.ac.jp>
2021-10-29 17:16:47 +0900

    fix: 赤点とスポットが表示されないバグを解決

--- lib/map_painter.dart
+++ lib/map_painter.dart
@@ -40,63 +40,60 @@ class MapPainter extends CustomPainter {
     final dst = Rect.fromLTWH(0, 0, size.width, size.height); // 描画場所
     canvas.drawImageRect(_mapImage, src, dst, paint); // 背景マップの描画
 
-    /// 位置情報の取得
-    Position currentLocation = await getLocationInformation();
-
 
     // 場所ごとの処理
     for (var item in _mapItems) {
       // 表示する画像の取得
       final img = item.getDisplayImage();
-      // もし画像がロードできていないなら何もしない
-      if (img != null) {
-        final length = min(img.height, img.width).toDouble(); // 縦横のうち最短を取得
-        // FIXME: 画像を中央に寄せる
-        final src = Rect.fromLTWH(0, 0, length, length); // 画像中の描画する場所を選択
-        final rescaleRect = item.getPhotoRectForDeviceFit(scale, _getMoveX()); // どこに描画するかを設定
 
-        /// 現在地とスポットの距離を計測する
+      if(img == null) {
+        // 画像がロードできていないなら何もしない
+        continue;
+      }
+
+      /// 円を書く
+      canvas.drawCircle(Offset(item.position.dx * scale - _getMoveX(), item.position.dy * scale), 10, paint);
+
+      /// 現在地とスポットの距離を計測する
+      Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.bestForNavigation).then((currentLocation) {
         YInari = Geolocator.distanceBetween(currentLocation.latitude, currentLocation.longitude, 36.4856770, 136.7582343);
         Souyu = Geolocator.distanceBetween(currentLocation.latitude, currentLocation.longitude, 36.48567221199191, 136.75751246063845);
+      });
 
-        // 円を書く
-        canvas.drawCircle(Offset(item.position.dx * scale - _getMoveX(),
-            item.position.dy * scale), 10, paint);
+      final length = min(img.height, img.width).toDouble(); // 縦横のうち最短を取得
+      // FIXME: 画像を中央に寄せる
+      final src = Rect.fromLTWH(0, 0, length, length); // 画像中の描画する場所を選択
+      final rescaleRect = item.getPhotoRectForDeviceFit(scale, _getMoveX()); // どこに描画するかを設定
+      /// 写真(イラストを表示)
+      canvas.drawImageRect(img, src, rescaleRect, paint);
 
-        if (YInari < 30 && item.name == "湯涌稲荷神社") {
-          print("==================================================");
-          print(YInari);
+      if (YInari < 30 && item.name == "湯涌稲荷神社") {
+        print("==================================================");
+        print(YInari);
 
-          // 線をひく
-          canvas.drawLine(Offset((item.photoRect.left * scale - _getMoveX()) +
-              item.photoRect.width * scale / 2,
-              (item.photoRect.top * scale) +
-                  item.photoRect.height * scale / 2),
+        // 線をひく
+        canvas.drawLine(Offset((item.photoRect.left * scale - _getMoveX()) +
+            item.photoRect.width * scale / 2,
+            (item.photoRect.top * scale) +
+                item.photoRect.height * scale / 2),
 
-              Offset((item.position.dx * scale - _getMoveX()),
-                  item.position.dy * scale), paint);
-        }
-        if (Souyu < 30 && item.name == "総湯") {
-          print("==================================================");
-          print(YInari);
+            Offset((item.position.dx * scale - _getMoveX()),
+                item.position.dy * scale), paint);
+      }
+      if (Souyu < 30 && item.name == "総湯") {
+        print("==================================================");
+        print(YInari);
 
-          // 線をひく
-          canvas.drawLine(Offset((item.photoRect.left * scale - _getMoveX()) +
-              item.photoRect.width * scale / 2,
-              (item.photoRect.top * scale) +
-                  item.photoRect.height * scale / 2),
+        // 線をひく
+        canvas.drawLine(Offset((item.photoRect.left * scale - _getMoveX()) +
+            item.photoRect.width * scale / 2,
+            (item.photoRect.top * scale) +
+                item.photoRect.height * scale / 2),
 
-              Offset((item.position.dx * scale - _getMoveX()),
-                  item.position.dy * scale), paint);
-        }
-
-        // canvas.drawShadow(path,Colors.black54, 20,);
-        // 写真(イラストを表示)
-        canvas.drawImageRect(img, src, rescaleRect, paint);
+            Offset((item.position.dx * scale - _getMoveX()),
+                item.position.dy * scale), paint);
       }
     }
-
-
   }
 
   @override
@@ -104,10 +101,6 @@ class MapPainter extends CustomPainter {
     return true;
   }
 
-
-  getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-  }
 
   static Future<void> openAppSettings() async {
     /// 設定画面に遷移
