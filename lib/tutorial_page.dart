b947c3b3845925afb776b3de538e8c5d92427233
yamanetaisei <swiftmantaisei@gmail.com>
2021-12-01 15:12:08 +0900

    PageViewを追加

--- lib/tutorial_page.dart
+++ lib/tutorial_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' as prefix;
 
 class TutorialPage extends StatefulWidget {
   @override
@@ -10,9 +11,31 @@ class TutorialPage extends StatefulWidget {
 class _TutorialPageState extends State<TutorialPage> {
   @override
   Widget build(BuildContext context) {
+
+    final PageController controller = PageController(initialPage: 0);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("title"),
+        title: Text("使い方ガイド",
+          style: TextStyle(color: prefix.Colors.black87)
+        ),
+      ),
+      body: Center(
+        child: PageView(
+          scrollDirection: Axis.horizontal,
+          controller: controller,
+          children: const <Widget>[
+            Center(
+              child: Text('First Page'),
+            ),
+            Center(
+              child: Text('Second Page'),
+            ),
+            Center(
+              child: Text('Third Page'),
+            )
+          ],
+        ),
       ),
     );
   }
