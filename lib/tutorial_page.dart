2d03065210f231a8f821f31d06ed848c9837f877
yamanetaisei <swiftmantaisei@gmail.com>
2021-12-02 22:24:02 +0900

    tutorialページ下部のindicator(ページを表す点々)を実装

--- lib/tutorial_page.dart
+++ lib/tutorial_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart' as prefix;
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:yuwaku_proto/tutorial_step_page.dart';
 
 class TutorialPage extends StatefulWidget {
@@ -22,15 +23,40 @@ class _TutorialPageState extends State<TutorialPage> {
         ),
       ),
       body: Center(
-        child: PageView(
-          scrollDirection: Axis.horizontal,
-          controller: controller,
-          children: <Widget>[
-            Center(
-              child: TutorialStepPage(PageData.first),
+        child: Stack(
+          children: [
+            PageView(
+              scrollDirection: Axis.horizontal,
+              controller: controller,
+              children: <Widget>[
+                Center(
+                  child: TutorialStepPage(PageData.first),
+                ),
+                Center(
+                  child: TutorialStepPage(PageData.second),
+                ),
+              ],
             ),
-            Center(
-              child: TutorialStepPage(PageData.second),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Container(
+                    child: Center(
+                      child: SmoothPageIndicator(
+                        controller: controller,
+                        count: 2,
+                        effect: WormEffect(
+                          dotColor: Colors.grey,
+                          activeDotColor: Color.fromRGBO(186, 66, 43, 100)
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
