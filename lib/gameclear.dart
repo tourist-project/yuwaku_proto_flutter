9990cb6c592d1fa9af8d0d585963c9d064a79496
北川 楓雅 <82246562+KitagawaFuuga@users.noreply.github.com>
2021-12-17 20:03:32 +0900

    Update gameclear.dart
    画面サイズを直しました

--- lib/gameclear.dart
+++ lib/gameclear.dart
@@ -14,88 +14,88 @@ class clearpage extends StatelessWidget {
     'assets/images/KeigoSirayu.png',
     'assets/images/map_img.png'
   ];
-  
+
   List<String> posName = ["稲荷神社", "湯涌総湯中", "湯涌総湯外", "湯涌全体図"];
-  
-   _launchURL() async {
+
+  _launchURL() async {
     const url = "https://totteku.tourism-project.com/";
     if (await canLaunch(url)) {
-      await launch(url);
+await launch(url);
     }
   }
 
-    // 写真を表示する
-    Widget photogoal(double top, double left, var image) {
-      return Container(
-        width: width / 2.125,
-        height: height / 5.8,
-        margin: EdgeInsets.only(top: top, left: left),
-        child: Image(image: AssetImage(image),
-        fit: BoxFit.cover),
-      );
-    }
+  // 写真を表示する
+  Widget photogoal(double top, double left, var image) {
+    return Container(
+      width: width / 2.125,
+      height: height / 5.8,
+      margin: EdgeInsets.only(top: top, left: left),
+      child: Image(image: AssetImage(image), fit: BoxFit.cover),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Test'),
+        title: Text('Test'),
       ),
       body: Container(
         color: Color.fromRGBO(240, 233, 208, 1),
-        child:  Column(
-        children: [
-          Row(
-            children: <Widget>[
-              photogoal(height / 100, width/70, imagephoto[0]),
-              photogoal(height / 100, width/30, imagephoto[1]),
-            ],
-          ),
-          Row(children: <Widget>[
-            photogoal(height / 100, width / 70, imagephoto[2]),
-            photogoal(height / 100, width / 30, imagephoto[3]),
-          ]),
-          Container(
-            margin: EdgeInsets.only(top: height / 100),
-            height: height / 3.5,
-            width: width / 1.02,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(color: Color.fromRGBO(186, 66, 43, 1), width: 2),
+        child: Column(
+          children: [
+            Row(
+              children: <Widget>[
+                photogoal((height / 100), width / 70, imagephoto[0]),
+                photogoal(height / 100, width / 30, imagephoto[1]),
+              ],
             ),
-            padding: const EdgeInsets.all(8),
-
-            child:  Scrollbar(
-              child: SingleChildScrollView(
-                child: Text(
-                  ('TestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest'),
-                  style: TextStyle(fontSize: height / 40),
+            Row(children: <Widget>[
+              photogoal(height / 100, width / 70, imagephoto[2]),
+              photogoal(height / 100, width / 30, imagephoto[3]),
+            ]),
+            Container(
+              margin: EdgeInsets.only(top: height / 100),
+              height: height / 3.8,
+              width: width / 1.02,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Color.fromRGBO(186, 66, 43, 1), width: 2),
+              ),
+              padding: const EdgeInsets.all(8),
+              child: Scrollbar(
+                child: SingleChildScrollView(
+                  child: Text(
+                    ('TestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest'),
+                    style: TextStyle(fontSize: height / 40),
+                  ),
                 ),
               ),
             ),
-          ),
-          Container(
-            height: height / 15,
-            width: width / 1.02,
-            margin: EdgeInsets.only(top: 10),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(color: Color.fromRGBO(186, 66, 43, 1), width: 2),
-            ),
-            child: TextButton(
-              child: Text(
-                'フォトコンページに行く',
-                style: TextStyle(fontSize: height / 30, color: Colors.black),
+            Container(
+              height: height / 15,
+              width: width / 1.02,
+              margin: EdgeInsets.only(top: 10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Color.fromRGBO(186, 66, 43, 1), width: 2),
               ),
-              onPressed: () {
-                _launchURL();
-              },
+              child: TextButton(
+                child: FittedBox(
+                  fit: BoxFit.fitWidth,
+                  child: Text(
+                    'フォトコンページに行く',
+                    style: TextStyle(fontSize: height / 30, color: Colors.black),
+                  ),
+                ),
+                onPressed: () {
+                  _launchURL();
+                },
+              ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
-    ),
     );
   }
 }
-
