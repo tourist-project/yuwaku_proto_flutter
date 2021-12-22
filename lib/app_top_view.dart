969916bc5f0498d4154cdccd82a5f7403c9655d3
keigo <b1951154@planet.kanazawa-it.ac.jp>
2021-12-22 21:19:01 +0900

    全てExpandedで囲った

--- lib/app_top_view.dart
+++ lib/app_top_view.dart
@@ -48,205 +48,212 @@ class topPageView extends StatelessWidget{
 
         child: Column(
           children: <Widget>[
-            Opacity(opacity: 0.6,
-              child: Container(
-                height: mediaHeight/12,
-                width: mediaWidth,
-                color: prefix.Colors.black,
-                child: SizedBox(
-                  width: mediaWidth/2,
-                  child: FittedBox(
-                    child: Text("2022年1月30日",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontStyle: FontStyle.italic,
-                        color: prefix.Colors.white,
+            Expanded( // 日付
+              flex: 4,
+              child: Opacity(opacity: 0.6,
+                child: Container(
+                  height: mediaHeight/12,
+                  width: mediaWidth,
+                  color: prefix.Colors.black,
+                  child: SizedBox(
+                    width: mediaWidth/2,
+                    child: FittedBox(
+                      child: Text("2022年1月30日",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontStyle: FontStyle.italic,
+                          color: prefix.Colors.white,
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
             ),
-            Spacer(),
 
-            Container(
-              width: mediaWidth,
-              height: mediaHeight/3,
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    child: Text("湯涌\nフォトラリー",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: prefix.Colors.white,
-                        fontSize: mediaWidth/7,
-                        fontWeight: FontWeight.bold,
+            Expanded( //フォトラリーWidget
+              flex: 12,
+              child: Container(
+                width: mediaWidth,
+                height: mediaHeight/3,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      child: Text("湯涌\nフォトラリー",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: prefix.Colors.white,
+                          fontSize: mediaWidth/7,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
-                  ),
-                  Spacer(),
-                  Container(
-                    child: Text("写真を撮ってスタンプラリー",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: prefix.Colors.white,
-                        fontSize: mediaWidth/25,
-                        fontWeight: FontWeight.bold,
+                    Spacer(),
+                    Container(
+                      child: Text("写真を撮ってスタンプラリー",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: prefix.Colors.white,
+                          fontSize: mediaWidth/25,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
-                  ),
-                  Spacer(),
-                ],
+                    Spacer(),
+                  ],
+                ),
               ),
             ),
 
-            Container(
-              child: Row(
-                children: <Widget>[
-                  Spacer(),
-                  RawMaterialButton(
-                    onPressed: () {
-                      Navigator.of(context).push(
+            Expanded( // ボタン配置
+              flex: 8,
+              child: Container(
+                child: Row(
+                  children: <Widget>[
+                    Spacer(),
+                    RawMaterialButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
                           MaterialPageRoute(
                             builder: (context) {
                               return TutorialPage();
-                            },
+                              },
                           ),
-                      );
-                    },
-                    shape: CircleBorder(),
-                    child: Container(
-                      alignment: Alignment.center,
-                      width: mediaWidth/5,
-                      height: mediaHeight/8,
-                      decoration: const BoxDecoration(
+                        );
+                        },
+                      shape: CircleBorder(),
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: mediaWidth/5,
+                        height: mediaHeight/8,
+                        decoration: const BoxDecoration(
                           shape: BoxShape.circle,
                           color: prefix.Colors.lightBlueAccent,
-                      ),
-                      child:const Text("遊び方",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: prefix.Colors.white
                         ),
-                      ),
-                    ),
-                  ),
-                  Spacer(),
-                  RawMaterialButton(
-                    onPressed: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return MapPage(title: '地図');
-                          },
-                        ),
-                      );
-                    },
-                    shape: CircleBorder(),
-                    child: Container(
-                      alignment: Alignment.center,
-                      width: mediaWidth/5,
-                      height: mediaHeight/8,
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: prefix.Colors.redAccent
-                      ),
-                      child: const Text("スタート",
+                        child:const Text("遊び方",
                           textAlign: TextAlign.center,
                           style: TextStyle(
                               color: prefix.Colors.white
                           ),
+                        ),
                       ),
                     ),
-                  ),
-                  Spacer(),
-                  RawMaterialButton(
-                    onPressed: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return PicExplain(title: '場所説明');
-                          },
+                    Spacer(),
+                    RawMaterialButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return MapPage(title: '地図');
+                              },
+                          ),
+                        );
+                        },
+                      shape: CircleBorder(),
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: mediaWidth/5,
+                        height: mediaHeight/8,
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: prefix.Colors.redAccent
                         ),
-                      );
+                        child: const Text("スタート",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: prefix.Colors.white
+                          ),
+                        ),
+                      ),
+                    ),
+                    Spacer(),
+                    RawMaterialButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return PicExplain(title: '場所説明');
+                              },
+                          ),
+                        );
+                        },
+                      shape: CircleBorder(),
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: mediaWidth/5,
+                        height: mediaHeight/8,
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: prefix.Colors.orange
+                        ),
+                        child: const Text("スポット",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: prefix.Colors.white
+                          ),
+                        ),
+                      ),
+                    ),
+                    Spacer(),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 4,
+                child: Container( // フォトコン
+                  width: mediaWidth/1.3,
+                  height: mediaHeight/11,
+                  alignment: Alignment.center,
+                  decoration: const BoxDecoration( // 背景
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/photoConPage.png'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Text("フォトコンテスト",
+                    style: TextStyle(
+                        color: prefix.Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: mediaWidth/16
+                    ),
+                  ),
+                ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                color: Colors.red,
+              ),
+            ),
+
+            Expanded( //webサイトに飛ぶ
+                flex: 3,
+                child: SizedBox(
+                  width: mediaWidth/1.2,
+                  height: mediaHeight/15,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: prefix.Colors.red[300],
+                      onPrimary: prefix.Colors.white,
+                      shape: const StadiumBorder(),
+                    ),
+                    onPressed: () { // ここにWebサイトに飛ぶ処理
+                      _launchURLtoWebSite();
                     },
-                    shape: CircleBorder(),
-                    child: Container(
-                      alignment: Alignment.center,
-                      width: mediaWidth/5,
-                      height: mediaHeight/8,
-                      decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: prefix.Colors.orange
-                      ),
-                      child: const Text("スポット",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: prefix.Colors.white
-                        ),
-                      ),
+                    child: Text(
+                      "Webサイトへ",
                     ),
                   ),
-                  Spacer(),
-                ],
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                height: mediaHeight/25,
-                // color: prefix.Colors.blue, //確認用
-              ),
-            ),
-            Container(
-              width: mediaWidth/1.3,
-              height: mediaHeight/11,
-              alignment: Alignment.center,
-              decoration: const BoxDecoration( // 背景
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/photoConPage.png'),
-                    fit: BoxFit.cover,
-                  ),
-              ),
-              child: Text("フォトコンテスト",
-                style: TextStyle(
-                  color: prefix.Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: mediaWidth/16
                 ),
-              ),
             ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                height: mediaHeight/20,
-                // color: prefix.Colors.blue, //確認用
-              ),
-            ),
-            SizedBox(
-              width: mediaWidth/1.2,
-              height: mediaHeight/15,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: prefix.Colors.red[300],
-                  onPrimary: prefix.Colors.white,
-                  shape: const StadiumBorder(),
-                ),
-                onPressed: () { // ここにWebサイトに飛ぶ処理
-                  _launchURLtoWebSite();
-                },
-                child: Text(
-                  "Webサイトへ",
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 3,
+            Expanded( // 下タブバーの裏にある空間
+              flex: 4,
                 child: Container(
-                  height: mediaHeight/8,
-                  // color: prefix.Colors.blue, //確認用
+                  color: Colors.red,
                 ),
             ),
+
           ],
         ),
       ),
