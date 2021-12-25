f5d49d17bbb6389d5538dd7cea0d9d14bd736c49
北川 楓雅 <82246562+KitagawaFuuga@users.noreply.github.com>
2021-12-25 10:09:05 +0900

    Update gameclear.dart
    指摘された部分を改良しました。しかし、データベースからの写真の参照が出来ていないのでお願いします。

--- lib/gameclear.dart
+++ lib/gameclear.dart
@@ -4,7 +4,6 @@ import 'package:url_launcher/url_launcher.dart';
 
 class clearpage extends StatelessWidget {
   double width, height;
-
   clearpage(this.width, this.height);
 
   // 表示する写真を入れる
@@ -15,24 +14,23 @@ class clearpage extends StatelessWidget {
     'assets/images/map_img.png'
   ];
 
-  List<String> posName = ["稲荷神社", "湯涌総湯中", "湯涌総湯外", "湯涌全体図"];
-
-  _launchURL() async {
+  Future<void> _launchURL() async {
     const url = "https://totteku.tourism-project.com/";
     if (await canLaunch(url)) {
-await launch(url);
+      await launch(url);
     }
   }
 
   // 写真を表示する
-  Widget photogoal(double top, double left, var image) {
-    return Container(
-      width: width / 2.125,
-      height: height / 5.8,
-      margin: EdgeInsets.only(top: top, left: left),
-      child: Image(image: AssetImage(image), fit: BoxFit.cover),
+  Widget photogoal(final image) {
+    return Expanded(
+      child: Container(
+        padding: const EdgeInsets.all(5),
+        child: Image(image: AssetImage(image), fit: BoxFit.cover),
+      ),
     );
   }
+  
 
   @override
   Widget build(BuildContext context) {
@@ -44,53 +42,65 @@ await launch(url);
         color: Color.fromRGBO(240, 233, 208, 1),
         child: Column(
           children: [
-            Row(
-              children: <Widget>[
-                photogoal((height / 100), width / 70, imagephoto[0]),
-                photogoal(height / 100, width / 30, imagephoto[1]),
-              ],
-            ),
-            Row(children: <Widget>[
-              photogoal(height / 100, width / 70, imagephoto[2]),
-              photogoal(height / 100, width / 30, imagephoto[3]),
-            ]),
-            Container(
-              margin: EdgeInsets.only(top: height / 100),
-              height: height / 3.8,
-              width: width / 1.02,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Color.fromRGBO(186, 66, 43, 1), width: 2),
+            Expanded(
+              flex: 2,
+              child: Row(
+                children: <Widget>[
+                  photogoal(imagephoto[0]),
+                  photogoal(imagephoto[1]),
+                ],
               ),
-              padding: const EdgeInsets.all(8),
-              child: Scrollbar(
-                child: SingleChildScrollView(
-                  child: Text(
-                    ('TestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest'),
-                    style: TextStyle(fontSize: height / 40),
+            ),
+            Expanded(
+              flex: 2,
+              child: Row(children: <Widget>[
+                photogoal(imagephoto[2]),
+                photogoal(imagephoto[3]),
+              ]),
+            ),
+            Expanded(
+              flex: 5,
+              child: Container(
+                margin: EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                      color: Color.fromRGBO(186, 66, 43, 1), width: 2),
+                ),
+                padding: const EdgeInsets.all(8),
+                child: Scrollbar(
+                  child: SingleChildScrollView(
+                    child: Text(
+                      ('TestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTestTest'),
+                      style: TextStyle(fontSize: height / 40),
+                    ),
                   ),
                 ),
               ),
             ),
-            Container(
-              height: height / 15,
-              width: width / 1.02,
-              margin: EdgeInsets.only(top: 10),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Color.fromRGBO(186, 66, 43, 1), width: 2),
-              ),
-              child: TextButton(
-                child: FittedBox(
-                  fit: BoxFit.fitWidth,
-                  child: Text(
-                    'フォトコンページに行く',
-                    style: TextStyle(fontSize: height / 30, color: Colors.black),
-                  ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                margin: EdgeInsets.all(5),
+                width: 400,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                      color: Color.fromRGBO(186, 66, 43, 1), width: 2),
                 ),
-                onPressed: () {
-                  _launchURL();
-                },
+                child: TextButton(
+                  child: FittedBox(
+                    fit: BoxFit.fitWidth,
+                    child: Text(
+                      'フォトコンページに行く',
+                      style:
+                          TextStyle(fontSize: height / 30, color: Colors.black),
+                    ),
+                  ),
+                  onPressed: () {
+                     _launchURL();
+                  },
+                ),
               ),
             ),
           ],
