f078d26305b86b0f07216292692da6f71b4a585a
北川 楓雅 <82246562+KitagawaFuuga@users.noreply.github.com>
2022-06-25 21:08:56 +0900

    Update some_top_page.dart

--- lib/some_top_page.dart
+++ lib/some_top_page.dart
@@ -1,5 +1,4 @@
 import 'dart:ui';
-
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
@@ -38,59 +37,57 @@ class _RunTopPage extends State<RunTopPage> {
       '氷室小屋は冷蔵施設がなく氷が大変貴重であった江戸時代に、大寒の雪を詰め'
           '天然の雪氷を夏まで長期保存するために作られた小屋です。湯涌ではこの雪詰めを体験'
           'できるイベントが開催されます。',
-
-      'assets/images/HimuroGoya/HimuroGoya.png',
-      36.48346516395541, 136.75701193508996,
+      'assets/images/HimuroGoya.png',
+      36.48346516395541,
+      136.75701193508996,
     ),
     HomePageItem(
         '金沢夢二館',
         "KanazawaYumejikan",
         '大正時代を代表する詩人画家の竹下夢二の記念館です。旅、女性、信仰心の3つ'
             'のテーマから、遺品や作品を通して夢二の芸術性や人間性を紹介しています。',
-
-        'assets/images/Yumezikan/Yumezikan.png',
-        36.48584951599308, 136.75738876226737
-    ),
-    HomePageItem('総湯', "Soyu",
+        'assets/images/Yumezikan.png',
+        36.48584951599308,
+        136.75738876226737),
+    HomePageItem(
+      '総湯',
+      "Soyu",
       '湯涌温泉の日帰り温泉。浴室はガラス窓であり、内湯でも開放的な気分になります。'
           '観光客だけでなく地元の方々にも日々利用されている名湯になります。',
-      'assets/images/Soyu/KeigoSirayu.png',
-      36.485425901995455, 136.75758738535384,
+      'assets/images/KeigoSirayu.png',
+      36.485425901995455,
+      136.75758738535384,
     ),
     HomePageItem(
         '足湯',
         "Ashiyu",
         '湯涌に2つある足湯の1つです。足だけの入浴なので無理なく体をしんから温める'
             'ことができます。無料なのでぜひ足湯を体験してみていかかでしょう。',
-
-        'assets/images/Ashiyu/Asiyu(temp).png',
-        36.48582537854954, 136.7574341842218
+        'assets/images/Asiyu(temp).png',
+        36.48582537854954,
+        136.7574341842218),
+    HomePageItem(
+      '薬師寺',
+      "Yakushiji Temple",
+      'testtesttesttesttesttesttesttesttesttesttesttest'
+          'testtesttesttesttesttesttesttesttesttest',
+      'assets/images/MidorinoSato.png',
+      36.48566,
+      136.75794,
     ),
-    HomePageItem('薬師寺', "Yakushizi",
-      '蕎麦打ち体験や梨の収穫体験などの様々なイベントが1年を通して行われ,'
-          '4月中旬〜12月中旬の毎週日曜日と水曜日に朝市が開催され新鮮な農作物などをお買い求めいただけます。',
-      'assets/images/Yakushizi1.png',
-      36.48566, 136.75794,
-    ),
-
-    //
-    // HomePageItem('みどりの里', "Midorinosato",
-    //   '蕎麦打ち体験や梨の収穫体験などの様々なイベントが1年を通して行われます。'
-    //       '4月中旬〜12月中旬の毎週日曜日と水曜日に朝市が開催され新鮮な農作物などをお買い求めいただけます。',
-    //   'assets/images/MidorinoSato.png',
-    //   36.49050881078798, 136.75404574490975,
-    // ),
   ];
 
   Stream<HomePageItem> _getStream() async* {
     MapPainter.determinePosition().then(
       (_) {
         Geolocator.getPositionStream(
-          intervalDuration: Duration(seconds: 10),
+          intervalDuration: Duration(seconds: 5),
           desiredAccuracy: LocationAccuracy.best,
         ).listen(
           (location) {
             for (var item in homeItems) {
+              print(location);
+              print('itemDistの距離${item.distance}');
               item.setDistance(location); // 距離関係を更新する
             }
           },
@@ -112,7 +109,7 @@ class _RunTopPage extends State<RunTopPage> {
 
     return SafeArea(
       child: StreamBuilder<HomePageItem>(
-        stream: _getStream(), //定期的に呼び出し
+        stream: _getStream(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
             return Scaffold(
@@ -293,7 +290,6 @@ class HomeClassTitleComponents extends StatelessWidget {
                     child: Column(
                       children: [
                         Container(
-                            height: heightSize / 20,
                             margin:  EdgeInsets.only(right: 5, left: 5),
                             child: AutoSizeText('目的地まで',
                                 style: TextStyle(fontSize: widthSize / 20),
@@ -316,7 +312,7 @@ class HomeClassTitleComponents extends StatelessWidget {
                                   ),
                                 ),
                               ),
-                        Flexible(
+                        Expanded(
                           flex: 1,
                           child: Container(
                             margin: const EdgeInsets.all(5),
@@ -360,7 +356,10 @@ class HomeClassTitleComponents extends StatelessWidget {
                                   children: [
                                     Icon(Icons.photo_camera_outlined,
                                         size: widthSize / 8),
-                                    AutoSizeText('タップで写真ページへ')
+                                    AutoSizeText(
+                                        'タップで写真ページへ',
+                                        maxLines: 1
+                                    )
                                   ],
                                 ),
                               ),
