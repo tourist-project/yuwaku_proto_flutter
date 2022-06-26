188a4e2840032ee711883d6fe50330a2076ba90c
taisei <swiftmantaisei@gmail.com>
2022-06-26 21:57:03 +0900

    CheckmarkNotifierのリスナーを全スポット分追加

--- lib/checkmark_notifier.dart
+++ lib/checkmark_notifier.dart
@@ -3,9 +3,33 @@ import 'package:flutter/material.dart';
 class CheckmarkNotifier extends ChangeNotifier {
 
   var isTakedHimurogoya = false;
+  var isTakedYumejikan = false;
+  var isTakedSoyu = false;
+  var isTakedAshiyu = false;
+  var isTakedYakushiji = false;
 
   void notifyTakedHimurogoya() {
     isTakedHimurogoya = true;
+    notifyListeners();
+  }
+
+  void notifyTakedYumejikan() {
+    isTakedYumejikan = true;
+    notifyListeners();
+  }
+
+  void notifyTakedSoyu() {
+    isTakedSoyu = true;
+    notifyListeners();
+  }
+
+  void notifyTakedAshiyu() {
+    isTakedAshiyu = true;
+    notifyListeners();
+  }
+
+  void notifyTakedYakushiji() {
+    isTakedYakushiji = true;
     notifyListeners();
   }
 }
