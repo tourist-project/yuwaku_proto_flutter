f6447c0acc79adbafeba51236e1ebd9dcb7519ce
北川 楓雅 <82246562+KitagawaFuuga@users.noreply.github.com>
2022-06-26 11:03:30 +0900

    Update some_top_page.dart

--- lib/some_top_page.dart
+++ lib/some_top_page.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
@@ -46,35 +47,31 @@ class _RunTopPage extends State<RunTopPage> {
         "KanazawaYumejikan",
         '大正時代を代表する詩人画家の竹下夢二の記念館です。旅、女性、信仰心の3つ'
             'のテーマから、遺品や作品を通して夢二の芸術性や人間性を紹介しています。',
-        'assets/images/Yumezikan.png',
-        36.48584951599308,
-        136.75738876226737),
+        'assets/images/Yumezikan/Yumezikan.png',
+        36.48584951599308, 136.75738876226737
+    ),
     HomePageItem(
       '総湯',
       "Soyu",
       '湯涌温泉の日帰り温泉。浴室はガラス窓であり、内湯でも開放的な気分になります。'
           '観光客だけでなく地元の方々にも日々利用されている名湯になります。',
-      'assets/images/KeigoSirayu.png',
-      36.485425901995455,
-      136.75758738535384,
+      'assets/images/Soyu/KeigoSirayu.png',
+      36.485425901995455, 136.75758738535384,
     ),
     HomePageItem(
         '足湯',
         "Ashiyu",
         '湯涌に2つある足湯の1つです。足だけの入浴なので無理なく体をしんから温める'
             'ことができます。無料なのでぜひ足湯を体験してみていかかでしょう。',
-        'assets/images/Asiyu(temp).png',
-        36.48582537854954,
-        136.7574341842218),
-    HomePageItem(
-      '薬師寺',
-      "Yakushiji Temple",
-      'testtesttesttesttesttesttesttesttesttesttesttest'
-          'testtesttesttesttesttesttesttesttesttest',
-      'assets/images/MidorinoSato.png',
-      36.48566,
-      136.75794,
+        'assets/images/Ashiyu/Asiyu(temp).png',
+        36.48582537854954, 136.7574341842218
     ),
+    HomePageItem('薬師寺', "Yakushizi",
+      '蕎麦打ち体験や梨の収穫体験などの様々なイベントが1年を通して行われ,'
+          '4月中旬〜12月中旬の毎週日曜日と水曜日に朝市が開催され新鮮な農作物などをお買い求めいただけます。',
+      'assets/images/Yakushizi1.png',
+      36.48566, 136.75794,
+      )
   ];
 
   Stream<HomePageItem> _getStream() async* {
