efd3b3528abdd68161e96d02c532e18ae333d3d9
taisei <swiftmantaisei@gmail.com>
2022-06-27 21:59:21 +0900

    不要な部分を削除

--- lib/some_top_page.dart
+++ lib/some_top_page.dart
@@ -1,12 +1,9 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'dart:async';
 import 'package:yuwaku_proto/goal_listview_cell.dart';
 import 'package:yuwaku_proto/homepage_component/homePage_Item.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:yuwaku_proto/map_component/map_painter.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'checkmark_notifier.dart';
 import 'goal.dart';
@@ -70,196 +67,172 @@ class _RunTopPage extends State<RunTopPage> {
       )
   ];
 
-  Stream<HomePageItem> _getStream() async* {
-    MapPainter.determinePosition().then(
-      (_) {
-        Geolocator.getPositionStream(
-          intervalDuration: Duration(seconds: 5),
-          desiredAccuracy: LocationAccuracy.best,
-        ).listen(
-          (location) {
-            for (var item in homeItems) {
-              setState(() {
-                item.setDistance(location); // 距離関係を更新する
-              });
-            }
-          },
-        );
-      },
-    );
-  }
-
-  @override
-  void initState() {
-    _getStream();
-    super.initState();
-  }
-
-  @override
-  void dispose(){
-    _getStream();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
-    double heightSize = MediaQuery.of(context).size.height;
-    double widthSize = MediaQuery.of(context).size.width;
+    double heightSize = MediaQuery
+        .of(context)
+        .size
+        .height;
+    double widthSize = MediaQuery
+        .of(context)
+        .size
+        .width;
 
     return SafeArea(
-      child: StreamBuilder<HomePageItem>(
-        stream: _getStream(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return Scaffold(
-              body: ChangeNotifierProvider(
-                create: (_) => CheckmarkNotifier(),
-                child: SingleChildScrollView(
-                  child: Container(
-                    width: widthSize,
-                    height: heightSize * 3.1,
-                    child: Column(
-                      children: [
-                        Expanded(
-                          flex: 2,
-                          child: Container(
-                            margin: EdgeInsets.only(top: widthSize / 16, left: widthSize / 12),
-                            width: widthSize,
-                            height: heightSize / 16,
-                            child: AutoSizeText('写真一覧', style: TextStyle(fontSize: widthSize / 12)),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: Container(
-                            margin: EdgeInsets.only(left: widthSize / 12, right: widthSize / 12),
-                            width: widthSize,
-                            height: heightSize / 30,
-                            child: AutoSizeText('取った写真や、観光地の写真の一覧です。',
-                                style: TextStyle(fontSize:  widthSize/24)),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 8,
-                          child: Container(
-                            width: widthSize,
-                            height: heightSize/3,
-                            child: ListView.builder(
-                              scrollDirection: Axis.horizontal,
-                              itemCount: homeItems.length,
-                              itemBuilder: (BuildContext context, int index){
-                                return Container(
-                                  width: widthSize/2,
-                                  height: heightSize/3,
-                                  margin: EdgeInsets.all(5),
-                                  decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(20),
-                                    image: DecorationImage(
-                                      fit: BoxFit.cover,
-                                      image: AssetImage(homeItems[index].image),
-                                    ),
-                                  ),
-                                );
-                              },
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 2,
-                          child: Container(
-                            margin: EdgeInsets.only(top: widthSize / 12, left: widthSize / 12),
-                            width: widthSize,
-                            height: heightSize / 16,
-                            child: AutoSizeText('目標一覧', style: TextStyle(fontSize: widthSize / 12)),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: Container(
-                            // color: Colors.black,
-                            margin: EdgeInsets.only(left: widthSize / 12, right: widthSize / 12),
-                            width: widthSize,
-                            height: heightSize / 20,
-                            child: AutoSizeText(
-                              '目標一覧です。写真をタップすると観光地の説明、ヒントを見ることが出来ます。',
-                              style: TextStyle(fontSize: widthSize/10),
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          flex: 35,
-                          child: Container(
-                            margin: EdgeInsets.only(top: widthSize / 18),
-                            child: ListView(
-                              scrollDirection: Axis.vertical,
-                              physics: const NeverScrollableScrollPhysics(),
-                              children: [
-                                GoalListViewCell(
-                                    homeItems: homeItems[0],
-                                    heightSize: heightSize,
-                                    widthSize: widthSize,
-                                    errorGetDistance: homeItems[0].distance,
-                                    camera: camera,
-                                    isTookPicture: context.watch<CheckmarkNotifier>().isTakedHimurogoya,
-                                  goal: Goal.himurogoya,
-                                ),
-                                GoalListViewCell(
-                                    homeItems: homeItems[1],
-                                    heightSize: heightSize,
-                                    widthSize: widthSize,
-                                    errorGetDistance: homeItems[1].distance,
-                                    camera: camera,
-                                    isTookPicture: context.watch<CheckmarkNotifier>().isTakedYumejikan,
-                                    goal: Goal.yumejikan
-                                ),
-                                GoalListViewCell(
-                                    homeItems: homeItems[2],
-                                    heightSize: heightSize,
-                                    widthSize: widthSize,
-                                    errorGetDistance: homeItems[2].distance,
-                                    camera: camera,
-                                    isTookPicture: context.watch<CheckmarkNotifier>().isTakedSoyu,
-                                    goal: Goal.soyu
-                                ),
-                                GoalListViewCell(
-                                    homeItems: homeItems[3],
-                                    heightSize: heightSize,
-                                    widthSize: widthSize,
-                                    errorGetDistance: homeItems[3].distance,
-                                    camera: camera,
-                                    isTookPicture: context.watch<CheckmarkNotifier>().isTakedAshiyu,
-                                  goal: Goal.ashiyu
-                                ),
-                                GoalListViewCell(
-                                    homeItems: homeItems[4],
-                                    heightSize: heightSize,
-                                    widthSize: widthSize,
-                                    errorGetDistance: homeItems[4].distance,
-                                    camera: camera,
-                                    isTookPicture: context.watch<CheckmarkNotifier>().isTakedYakushiji,
-                                  goal: Goal.yakushiji,
-                                )
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
+        child: Scaffold(
+          body: ChangeNotifierProvider(
+            create: (_) => CheckmarkNotifier(),
+            child: SingleChildScrollView(
+              child: Container(
+                width: widthSize,
+                height: heightSize * 3.1,
+                child: Column(
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        margin: EdgeInsets.only(
+                            top: widthSize / 16, left: widthSize / 12),
+                        width: widthSize,
+                        height: heightSize / 16,
+                        child: AutoSizeText(
+                            '写真一覧', style: TextStyle(fontSize: widthSize / 12)),
+                      ),
                     ),
-                  ),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        margin: EdgeInsets.only(
+                            left: widthSize / 12, right: widthSize / 12),
+                        width: widthSize,
+                        height: heightSize / 30,
+                        child: AutoSizeText('取った写真や、観光地の写真の一覧です。',
+                            style: TextStyle(fontSize: widthSize / 24)),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 8,
+                      child: Container(
+                        width: widthSize,
+                        height: heightSize / 3,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: homeItems.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Container(
+                              width: widthSize / 2,
+                              height: heightSize / 3,
+                              margin: EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: AssetImage(homeItems[index].image),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        margin: EdgeInsets.only(
+                            top: widthSize / 12, left: widthSize / 12),
+                        width: widthSize,
+                        height: heightSize / 16,
+                        child: AutoSizeText(
+                            '目標一覧', style: TextStyle(fontSize: widthSize / 12)),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        // color: Colors.black,
+                        margin: EdgeInsets.only(
+                            left: widthSize / 12, right: widthSize / 12),
+                        width: widthSize,
+                        height: heightSize / 20,
+                        child: AutoSizeText(
+                          '目標一覧です。写真をタップすると観光地の説明、ヒントを見ることが出来ます。',
+                          style: TextStyle(fontSize: widthSize / 10),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 35,
+                      child: Container(
+                        margin: EdgeInsets.only(top: widthSize / 18),
+                        child: ListView(
+                          scrollDirection: Axis.vertical,
+                          physics: const NeverScrollableScrollPhysics(),
+                          children: [
+                            GoalListViewCell(
+                              homeItems: homeItems[0],
+                              heightSize: heightSize,
+                              widthSize: widthSize,
+                              errorGetDistance: homeItems[0].distance,
+                              camera: camera,
+                              isTookPicture: context
+                                  .watch<CheckmarkNotifier>()
+                                  .isTakedHimurogoya,
+                              goal: Goal.himurogoya,
+                            ),
+                            GoalListViewCell(
+                                homeItems: homeItems[1],
+                                heightSize: heightSize,
+                                widthSize: widthSize,
+                                errorGetDistance: homeItems[1].distance,
+                                camera: camera,
+                                isTookPicture: context
+                                    .watch<CheckmarkNotifier>()
+                                    .isTakedYumejikan,
+                                goal: Goal.yumejikan
+                            ),
+                            GoalListViewCell(
+                                homeItems: homeItems[2],
+                                heightSize: heightSize,
+                                widthSize: widthSize,
+                                errorGetDistance: homeItems[2].distance,
+                                camera: camera,
+                                isTookPicture: context
+                                    .watch<CheckmarkNotifier>()
+                                    .isTakedSoyu,
+                                goal: Goal.soyu
+                            ),
+                            GoalListViewCell(
+                                homeItems: homeItems[3],
+                                heightSize: heightSize,
+                                widthSize: widthSize,
+                                errorGetDistance: homeItems[3].distance,
+                                camera: camera,
+                                isTookPicture: context
+                                    .watch<CheckmarkNotifier>()
+                                    .isTakedAshiyu,
+                                goal: Goal.ashiyu
+                            ),
+                            GoalListViewCell(
+                              homeItems: homeItems[4],
+                              heightSize: heightSize,
+                              widthSize: widthSize,
+                              errorGetDistance: homeItems[4].distance,
+                              camera: camera,
+                              isTookPicture: context
+                                  .watch<CheckmarkNotifier>()
+                                  .isTakedYakushiji,
+                              goal: Goal.yakushiji,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            );
-          } else {
-            return Expanded(
-              flex: 20,
-              child: Container(
-                alignment: Alignment.center,
-                child: CircularProgressIndicator(),
-              ),
-            );
-          }
-        },
-      ),
+            ),
+          ),
+        ),
     );
   }
 }
