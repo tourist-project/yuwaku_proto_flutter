c55d020d215a37711ca069d2dbbb1c3bca037c5b
taisei <swiftmantaisei@gmail.com>
2022-06-27 21:14:10 +0900

    setState StreamBuilderで距離を更新

--- lib/distance_goal_text.dart
+++ lib/distance_goal_text.dart
@@ -1,26 +1,66 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:yuwaku_proto/map_component/map_painter.dart';
 
-class DistanceGoalText extends StatelessWidget {
+class DistanceGoalText extends StatefulWidget {
+  const DistanceGoalText({Key? key}) : super(key: key);
+
+  @override
+  State<DistanceGoalText> createState() => _DistanceGoalText();
+}
+
+class _DistanceGoalText extends State<DistanceGoalText> {
+
+  double latitude = 36.48346516395541;
+  double longitude = 136.75701193508996;
+  double? distance;
+
+  void setDistance(Position position) {
+    this.distance = Geolocator.distanceBetween(
+        position.latitude, position.longitude, this.latitude, this.longitude);
+  }
+
+  Stream<Position> _getDistance() async* {
+    MapPainter.determinePosition().then((_) {
+      Geolocator.getPositionStream(
+        intervalDuration: Duration(seconds: 5),
+        desiredAccuracy: LocationAccuracy.best,
+      ).listen((location) {
+        setState(() {
+          setDistance(location);
+        });
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-        children: [
-          Container(
-            margin: EdgeInsets.only(right: 5, left: 5),
-            child: AutoSizeText(
-              '目的地まで',
-              style: TextStyle(fontSize: 20),
-            ),
-          ),
-          Center(
-            child: AutoSizeText(
-              'あと' + '20' + 'm',
-              style: TextStyle(fontSize:  18),
-            ),
-          ),
-        ]
-    );
+    return StreamBuilder<Position>(
+        stream: _getDistance(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Column(children: [
+              Container(
+                margin: EdgeInsets.only(right: 5, left: 5),
+                child: AutoSizeText(
+                  '目的地まで',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              Center(
+                child: AutoSizeText(
+                  'あと' + '${distance}' + 'm',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ]);
+          } else {
+            return Container();
+          }
+        });
   }
 }
