cc3e607e8c0dffca9884d2a617c28bff2622614d
keigo <b1951154@planet.kanazawa-it.ac.jp>
2022-08-29 15:56:44 +0900

    サイドバーの色修正

--- lib/drawer_layout.dart
+++ lib/drawer_layout.dart
@@ -13,56 +13,68 @@ class DrawerLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      padding: EdgeInsets.all(0),
-      children: [
-        SizedBox(
-          height: 150,
-          child: DrawerHeader(
-            decoration: BoxDecoration(),
+    return Container(
+      color: Color.fromRGBO(240, 233, 208, 10),
+      child: ListView(
+        padding: EdgeInsets.all(0),
+        children: [
+          SizedBox(
+            height: 80,
             child: Container(
-                child: Image(
-                  image: AssetImage('assets/images/SideBarLog.png'),
-                )
+              child: DrawerHeader(
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Color.fromRGBO(186, 66, 43, 10),
+                      width: 2
+                    ),
+                  ),
+                ),
+                child: Container(
+                    child: Image(
+                      image: AssetImage('assets/images/SideBarLog.png'),
+                    )
+                ),
+              ),
             ),
           ),
-        ),
-        ListTile(
-          leading: Icon(Icons.launch),
-          title: const Text('Webサイト'),
-          onTap: () {
-            webSites.launchTourismURL();
-          },
-        ),
-        ListTile(
-          leading: Icon(Icons.camera_alt),
-          title: const Text('フォトコンテスト'),
-          onTap: () {
-            webSites.launchPhotoContestURL();
-          },
-        ),
-        ListTile(
-          leading: Icon(Icons.list_alt),
-          title: const Text('アンケート'),
-          onTap: () {
-            webSites.launchQuestionWebSURL();
-          },
-        ),
-        ListTile(
-          leading: Icon(Icons.insert_drive_file),
-          title: const Text('利用規約'),
-          onTap: () {
-
-          },
-        ),
-        ListTile(
-          leading: Icon(FontAwesomeIcons.twitter),
-          title: const Text('運営Twitter'),
-          onTap: () {
-            webSites.twitterURL();
-          },
-        ),
-      ],
+          ListTile(
+            leading: Icon(Icons.launch),
+            title: const Text('Webサイト'),
+            onTap: () {
+              webSites.launchTourismURL();
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.camera_alt),
+            title: const Text('フォトコンテスト'),
+            onTap: () {
+              webSites.launchPhotoContestURL();
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.list_alt),
+            title: const Text('アンケート'),
+            onTap: () {
+              webSites.launchQuestionWebSURL();
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.insert_drive_file),
+            title: const Text('利用規約'),
+            onTap: () {
+            },
+          ),
+          ListTile(
+            leading: Icon(FontAwesomeIcons.twitter),
+            title: const Text('運営Twitter'),
+            tileColor: Color.fromRGBO(240, 233, 208, 10),
+            onTap: () {
+              webSites.twitterURL();
+            },
+          ),
+        ],
+      ),
     );
   }
 }
