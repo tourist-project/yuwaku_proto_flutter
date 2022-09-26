e1ba1124fc7e450ec908a5d99d227e04eb82237f
keigo <b1951154@planet.kanazawa-it.ac.jp>
2022-09-26 15:26:14 +0900

    応募要項の文言入れ

--- lib/entry_photoContest_page.dart
+++ lib/entry_photoContest_page.dart
@@ -1,5 +1,8 @@
+import 'dart:ui';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:image/image.dart';
 import 'package:provider/provider.dart';
 import 'package:yuwaku_proto/goal_listview_cell.dart';
 import 'package:yuwaku_proto/homepage_component/homePage_Item.dart';
@@ -20,12 +23,24 @@ class PhotoContestEntry extends StatefulWidget {
 class _PhotoContestEntryState extends State<PhotoContestEntry> {
 
   Map<String, String> _photoContestExplain = {
-    'test1': '艦これ',
-    'test2': 'アズレン',
-    'test3': '艦これ1',
-    'test4': 'アズレン2',
-    'test5': '艦これ3',
-    'test5': 'アズレン4',
+    '応募期間': '2022/10/22 〜 2022/10/26 12:00',
+    '賞': '・湯涌ぼんぼり賞(1名)：金沢湯涌温泉氷室氷菓〜冬デザイン〜 1セット\n'
+        '・ゆわく隠れた魅力賞(1名)金沢湯涌サイダー 柚子乙女セット 1セット',
+    '応募資格': '湯涌ぼんぼり祭りに参加したこと',
+    '応募作品': '湯涌ぼんぼり祭り当日に撮影した写真(撮影媒体は問わない)',
+    '応募作品形式': '・ファイル形式：JPEGまたはPNG\n'
+        '・アップロード可能なサイズ：10MBまで',
+    '応募方法': '専用の応募フォームよりご応募ください',
+    '注意事項': '公的良俗に反していない写真の投稿をお願いします',
+    '審査方法': '・Tourism Projectと湯涌温泉観光協会の運営スタッフが以下の基準により審査致します\n'
+        '・湯涌ぼんぼり祭り：ぼんぼり祭りの魅力が最も伝えられている写真\n'
+        'ゆわく隠れた魅力賞：湯涌のちょっとした魅力を伝えるための写真',
+    '審査結果': '・入賞した場合のみ，応募事項に入力いただいたメールアドレスにご連絡致します\n'
+        '・2022/10/29に本サイトと湯涌町でポスターパネルにて発表させて頂きます\n'
+        '応募作品は入賞発表後フォトラリーページにて公開させて頂きます',
+    '応募作品の著作品・使用権': '・作品の使用の際には必ず応募時に応募者の入力されたお名前を掲載させて頂きます\n'
+        '・ご応募いただいた作品は湯涌写真コンテスト及び湯涌町の宣伝に使用し，厳正に管理致します\n'
+        '・著作権や肖像権について詳しくはこちらの利用規約に準じています．2次利用のある場合がございます．',
   };
 
   int _counter = 0;
@@ -45,32 +60,48 @@ class _PhotoContestEntryState extends State<PhotoContestEntry> {
       appBar: AppBar(
         title: Text('写真コンテスト', style: TextStyle(color: Colors.white)),
       ),
-      body: ListView.builder(
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            child: ListTile(
-              title: _textWidget(listJmp, index, _counter, 'title'),
-              subtitle: _textWidget(listJmp, index, _counter, 'explain'),
+      body: Column(
+        children: [
+          Container(
+            height: 200,
+            color: Colors.red,
+          ),
+          Flexible(
+            child: ListView.builder(
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  child: ListTile(
+                    title: _textWidget(listJmp, index, _counter, 'title'),
+                    subtitle: _textWidget(listJmp, index, _counter, 'explain'),
+                  ),
+                );
+              },
+              itemCount: _photoContestExplain.length,
             ),
-          );
-        },
-        itemCount: _photoContestExplain.length,
+          ),
+        ],
       ),
     );
   }
 }
 
 class PhotoContestExplain {
+
   String title;
   String explain;
-
   PhotoContestExplain(this.title, this.explain);
+
 }
 
 Widget _textWidget(List list, int index, int counter, String key) {
+
   if (key == 'title') {
     return Text(
       list[index].title,
+      style: TextStyle(
+          fontWeight: FontWeight.bold,
+
+      ),
     );
   }
   if (key == 'explain') {
@@ -80,6 +111,7 @@ Widget _textWidget(List list, int index, int counter, String key) {
   } else {
     return Text('nothing');
   }
+
 }
 
 
