512f607a36c42f76af5bee07a9397acc1430c182
KitagawaFuuga <c1104420@planet.kanazawa-it.ac.jp>
2023-01-21 16:00:09 +0900

    写真を撮るページのボタンが分かりにくかったので改善しました

--- lib/some_camera_page.dart
+++ lib/some_camera_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:flutter_launcher_icons/main.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:yuwaku_proto/display_picture_page.dart';
 import 'package:yuwaku_proto/goal.dart';
 import 'package:flutter/services.dart';
@@ -55,35 +57,57 @@ class _Camerapage extends State<Camerapage>{
 
   @override
   Widget build(BuildContext context) {
+    var Height = MediaQuery.of(context).size.height;
+    var Width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Center(
-        child: FutureBuilder<void>(
-          future: _initializeControllerFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return CameraPreview(_controller);
-            } else {
-              return const CircularProgressIndicator();
-            }
-          },
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          // 写真を撮る
-          final image = await _controller.takePicture();
-          SystemChrome.setPreferredOrientations([
-            DeviceOrientation.portraitUp,
-          ]);
-          // 表示用の画面に遷移
-          await Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => DisplayPicturePage(imagePath: image.path,camera: camera, goal: goal),
-              fullscreenDialog: true,
+      body: Container(
+        child: Stack(
+          children: [
+            Container(
+              height: Height,
+              width: Width,
+              child: FutureBuilder<void>(
+                future: _initializeControllerFuture,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    return CameraPreview(_controller);
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
+                },
+              ),
             ),
-            );
-        },
-        child: const Icon(Icons.photo_camera_outlined),
+            Align(
+              alignment: const Alignment(0, 0.9),
+              child: GestureDetector(
+                onTap: () async {
+                  // 写真を撮る
+                  final image = await _controller.takePicture();
+                  SystemChrome.setPreferredOrientations([
+                    DeviceOrientation.portraitUp,
+                  ]);
+                  // 表示用の画面に遷移
+                  await Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => DisplayPicturePage(imagePath: image.path,camera: camera, goal: goal),
+                      fullscreenDialog: true,
+                    ),
+                  );
+                },
+                child: Container( 
+                  width: Width / 4,
+                  height: Height / 7,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey, width: 8),
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                  ),
+                  child: Icon(Icons.photo_camera_outlined, size: Height / 20),
+                ),
+              ),
+            )
+          ],
+        ) 
       ),
     );
   }
